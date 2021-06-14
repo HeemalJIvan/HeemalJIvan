afbaa28143d0962645649f1f5db7a9b614d4230b
HeemalJIvan <85883924+HeemalJIvan@users.noreply.github.com>
2021-06-14 15:34:04 +0200

    Add files via upload
    iCompare wizard

---
+++ iCompare_Installer/iCompare_Installer/ClosingPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace iCompare_Installer
+{
+    /// <summary>
+    /// Interaction logic for ClosingPage.xaml
+    /// </summary>
+    public partial class ClosingPage : Window
+    {
+        public ClosingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFinish_Click(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+    }
+}
---
+++ iCompare_Installer/iCompare_Installer/iCompareSetup.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Fluid.Business.DataServer;
+using FusionXC.Data;
+using Microsoft.Data.ConnectionUI;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iCompare_Installer
+{
+    /// <summary>
+    /// Interaction logic for iCompareSetup.xaml
+    /// </summary>
+    public partial class iCompareSetup : Window
+    {
+       
+        readonly MainWindow main = new MainWindow();
+        readonly ClosingPage close = new ClosingPage();
+        public iCompareSetup()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            
+            main.Show();
+            this.Close();
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            close.Show();
+            this.Close();
+        }
+
+        string GetConnectionString()
+        {
+            DataConnectionDialog dcd = new DataConnectionDialog();
+
+            dcd.DataSources.Add(Microsoft.Data.ConnectionUI.DataSource.SqlDataSource);
+
+            if (DataConnectionDialog.Show(dcd) == System.Windows.Forms.DialogResult.OK)
+            {
+                return dcd.ConnectionString;
+            }
+
+            return String.Empty;
+        }
+        private void btnRetrieve_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
